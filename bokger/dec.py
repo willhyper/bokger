929fd8c178ac7e8e605d00b97582b6e2ba7da63c
willhyper <willhyper@gmail.com>
2023-10-30 11:31:03 -0700

    formatting args. +def _repr(*args, **kwargs) -> str

--- bokger/dec.py
+++ bokger/dec.py
@@ -2,22 +2,36 @@ from functools import wraps
 from .Logger import logger
 import time
 
-def log_io(fn, _log = logger.log):
+def _repr(*args, **kwargs) -> str:
+    _args = ', '.join([repr(a) for a in args])
+    _kwargs = ', '.join([f'{k}={v}' for k, v in kwargs.items()])
+    if _args and _kwargs:
+        return _args + ', ' + _kwargs
+    elif _args and not _kwargs:
+        return _args
+    elif not _args and _kwargs:
+        return _kwargs
+    else:
+        return ""
+
+
+def log_io(fn, _do = logger.log):
     @wraps(fn)
     def _dec(*args, **kwargs):
         _return = fn(*args, **kwargs)
-        _log(f"{fn.__name__}({args, kwargs})={_return}")
+        _do(f"{fn.__name__}({_repr(*args, **kwargs)})={_return}")
         return _return
     return _dec
 
-def log_time(fn, _log = logger.log):
+def log_time(fn, _do = logger.log):
     @wraps(fn)
     def _dec(*args, **kwargs):
         _start = time.time()        
         _return = fn(*args, **kwargs)
         _end = time.time()
         _elapsed = _end - _start
-        _log(f"{fn.__name__}({args, kwargs}) elapsed {_elapsed} seconds.")
+        
+        _do(f"{fn.__name__}({_repr(*args, **kwargs)}) elapsed {_elapsed} seconds.")
         return _return
     return _dec
 
